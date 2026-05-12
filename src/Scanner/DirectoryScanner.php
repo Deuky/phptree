b70af43fa0d61e031de7acc24b2a6c0afada0376
Yannick Gustin <ygustin@laptop.local>
2026-05-12 18:26:32 +0200

    fix 1.5

---
+++ src/Scanner/DirectoryScanner.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace PhpTree\Scanner;
+
+use SplFileInfo;
+use InvalidArgumentException;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
+use Generator;
+
+class DirectoryScanner
+{
+    public readonly array $excludes;
+
+    public function __construct(
+         $excludes = []
+    ){
+        $this->excludes = $excludes;
+    }
+
+    /**
+     * @return SplFileInfo[]
+     */
+    public function scan(string $directory): Generator
+    {
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(
+                $directory,
+                RecursiveDirectoryIterator::SKIP_DOTS,
+            ),
+            RecursiveIteratorIterator::SELF_FIRST,
+        );
+
+        /** 
+         * @var SplFileInfo $file
+         */
+        foreach ($iterator as $file) {
+            if (!$file->isFile()) {
+                continue;
+            }
+
+            if ($file->getExtension() !== 'php') {
+                continue;
+            }
+
+            if ($this->isExcluded($file->getPathname(), $directory)) {
+                continue;
+            }
+
+            yield $file; 
+        }
+    }
+
+    private function isExcluded(string $filePath, string $baseDir): bool
+    {
+        $relativePath = ltrim(explode($baseDir, $filePath)[1], ".".DIRECTORY_SEPARATOR);
+
+        return array_all(
+            $this->excludes,
+            fn($exclude) => str_starts_with($relativePath, $exclude)
+        );
+    }
+}
